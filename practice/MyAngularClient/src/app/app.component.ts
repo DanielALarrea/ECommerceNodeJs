b327626ca89c81d65d7c7c4e5a037d4100db50fd
DanielALarrea <daniellarrea2697@gmail.com>
2020-10-14 09:47:50 -0500

    Started on some guides connecting Angular and Node.JS

---
+++ practice/MyAngularClient/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title = 'MyAngularClient';
+  public isAuthenticated: boolean;
+
+  constructor() {
+    this.isAuthenticated = false;
+  }
+
+  login() {
+
+  }
+
+  logout() {
+    
+  }
+}
