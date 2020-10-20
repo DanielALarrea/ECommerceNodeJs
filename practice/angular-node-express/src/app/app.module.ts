35c5f675d1021ecbdb0c43e42744a69c9a3d1ea5
DanielALarrea <daniellarrea2697@gmail.com>
2020-10-20 14:52:10 -0500

    Practice connecting Angular and Node.JS - creating two components and with different paths

--- practice/angular-node-express/src/app/app.module.ts
+++ practice/angular-node-express/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Routes, RouterModule, Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InputUserDataFormComponent
+  },
+  {
+    path: 'user/:uid',
+    component: DisplayUserDataComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputUserDataFormComponent
+    InputUserDataFormComponent,
+    DisplayUserDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
